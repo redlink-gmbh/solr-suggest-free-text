f255c1eda29c1f9cb782d6446f2b71273ab8d607
Jakob Frank <jakob.frank@redlink.at>
2025-02-26 18:19:38 +0100

    chore: add dummy test

---
+++ src/test/java/io/redlink/lucene/search/suggest/FreeTextSuggesterTest.java
@@ -0,0 +1,13 @@
+package io.redlink.lucene.search.suggest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FreeTextSuggesterTest {
+
+    @Test
+    public void testPlaceholder() {
+        assertTrue("This is a dummy test", true);
+    }
+}
